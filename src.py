2adf57f68299c9a192624b3491e43d3ea4ba4b88
adeebkhan0706 <adeebkhan0706@gmail.com>
2021-05-28 22:44:07 +0530

    Final commit for console version

--- src.py
+++ src.py
@@ -3,20 +3,19 @@ from os import system
 n=int(input("Number of members in the group: -"))
 names=[]
 secret_santa_dict={}
+
 for i in range(0,n):
     name=input("Enter name of member:-")
     names.append(name)
+
 def santa_assigner():  
-    secret_santa_dict={}
     temp=names[:]
     for i in range(0,n):
-        while True:
-            assignee=random.choice(temp)
-            
-            if assignee==names[i]:
-                continue
-            else:
-                break
+        assignee=random.choice(temp)
+           
+        if assignee==names[i]:               
+            i=i-1
+            continue
 
         secret_santa_dict[names[i]]= assignee
         del temp[temp.index(assignee)]
@@ -24,15 +23,25 @@ def santa_assigner():
 """print(secret_santa_dict)"""
 
 def display(ch):
-    you_gift=secret_santa_dict[ch]
-    input("Hi "+ch+" press ENTER to see you gift")
-    print(you_gift)
+    _=system("clear")
+    you_gift=secret_santa_dict[str(ch)]
+    input("Hi "+ch+" press ENTER to see whose Secret Santa are you")
+    print("You are Secret Santa to "+you_gift)
     time.sleep(5)
     
 def runner():
     while True:
+        
         for i in range(0, n):
             _=system("clear")
             display(names[i])
             _=system("clear")
-        print("")
+        flag=input("If you want to repeat the process press ENTER")
+        time.sleep(5)
+        if flag == True:   
+            continue
+
+        else:
+            break
+santa_assigner()
+runner()
