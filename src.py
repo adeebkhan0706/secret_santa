dae080bad0879abbb2c3592c5fc72cdb533a4dbf
adeebkhan0706 <adeebkhan0706@gmail.com>
2021-05-28 13:46:18 +0530

    V-1.0 Final commit

--- src.py
+++ src.py
@@ -1,12 +1,24 @@
 import sys, random
-n=input("Number of members in the group")
+n=int(input("Number of members in the group: -"))
 names=[]
 for i in range(0,n):
-    name=input("Enter name of member",(i+1))
+    name=input("Enter name of member:-")
     names.append(name)
 
-secret_santa_dict=()
+secret_santa_dict={}
+temp=names[:]
 for i in range(0,n):
-    secret_santa_dict[names[i]]= random.choice(names)
+    while True:
+        assignee=random.choice(temp)
+        
+        if assignee==names[i]:
+            continue
+        else:
+            break
+
+    secret_santa_dict[names[i]]= assignee
+    del temp[temp.index(assignee)]
+
+
 
 print(secret_santa_dict)
